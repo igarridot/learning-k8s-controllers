e63a6bcb9e32bce0f8303c36d6ce479b1451104e
Iván Garrido Tamarit <33375539+igarridot@users.noreply.github.com>
2023-05-07 00:28:29 +0200

    Kubebuilder first steps (#1)
    * MVP, ugly and duplicated code. No real functionality
    
    * Exctract functionality and remove duplicated code
    
    * Modify gitignore
    
    * Add dummy credentials but more real flow

---
+++ mercacertmonger/internal/controller/certificate_controller.go
@@ -0,0 +1,157 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	tlsv1 "igarridot/learning-k8s-controllers/mercacertmonger/api/v1"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+type CertificateReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+const CertRenewalPeriod = 90
+const CertIsNearToExpire = 30
+
+//+kubebuilder:rbac:groups=tls.igarrido.io,resources=certificates,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=tls.igarrido.io,resources=certificates/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=tls.igarrido.io,resources=certificates/finalizers,verbs=update
+//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=secrets/status,verbs=get;update;patch
+
+// Here we go with the reconcile magic
+func (r *CertificateReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = log.FromContext(ctx)
+
+	fmt.Println("Starting reconciliation process")
+
+	// Load the Certificate by name
+	var certificate tlsv1.Certificate
+	if err := r.Get(ctx, req.NamespacedName, &certificate); err != nil {
+		fmt.Println(err, "unable to fetch Certificate")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	fmt.Println("Reconciliation process tarted for certificate ", certificate.Name)
+
+	// List all Secrets managed by the Certificate
+	var childSecrets corev1.SecretList
+	if err := r.List(ctx, &childSecrets, client.InNamespace(req.Namespace), client.MatchingFields{jobOwnerKey: req.Name}); err != nil {
+		fmt.Println(err, "unable to list child secrets")
+		return ctrl.Result{}, err
+	}
+
+	now := time.Now().Unix()
+
+	if certificate.Status.ValidTo == 0 {
+		fmt.Println("Creating certificate", certificate.Name)
+
+		CertificateTimestampManager(now, &certificate)
+		fmt.Println("Certificate", certificate.Name, "will be valid until: ", now+CertRenewalPeriod)
+
+		_, err := UpdateCertificateStatus(r, ctx, &certificate)
+		if err != nil {
+			fmt.Println("Cannot update Certificate Status", err)
+		}
+
+		secret, err := ConstructSecretForCertificate(&certificate, r)
+		if err != nil {
+			fmt.Println(err, "Unable to construct secret from template")
+			return ctrl.Result{}, nil
+		}
+		secret = ObtainTLSCertificate(secret)
+
+		_, err = CreateNewCertificateSecret(r, ctx, secret, &certificate)
+		if err != nil {
+			fmt.Println("Cannot create certificate", err)
+		}
+
+		fmt.Println("End of certificate", certificate.Name, "creation")
+	}
+
+	if CertificateNeedsToBeRenewed(now, certificate.Status.ValidTo, certificate) {
+		fmt.Println("Renewing certificate", certificate.Name)
+
+		CertificateTimestampManager(now, &certificate)
+		fmt.Println("Certificate", certificate.Name, "will be valid until: ", now+90)
+
+		_, err := UpdateCertificateStatus(r, ctx, &certificate)
+		if err != nil {
+			fmt.Println("Cannot update Certificate Status", err)
+		}
+
+		secret, err := ConstructSecretForCertificate(&certificate, r)
+		if err != nil {
+			fmt.Println(err, "Unable to construct secret from template")
+			return ctrl.Result{}, nil
+		}
+		secret = ObtainTLSCertificate(secret)
+
+		_, err = RenewCertificate(r, ctx, secret, &certificate)
+		if err != nil {
+			fmt.Println("Cannot renew certificate", err)
+		}
+
+		fmt.Println("End of certificate", certificate.Name, "renewal")
+	}
+
+	return ctrl.Result{}, nil
+}
+
+var (
+	jobOwnerKey = ".metadata.controller"
+	apiGVStr    = corev1.SchemeGroupVersion.String()
+)
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *CertificateReconciler) SetupWithManager(mgr ctrl.Manager) error {
+
+	if err := mgr.GetFieldIndexer().IndexField(context.Background(), &corev1.Secret{}, jobOwnerKey, func(rawObj client.Object) []string {
+		// grab the job object, extract the owner...
+		job := rawObj.(*corev1.Secret)
+		owner := metav1.GetControllerOf(job)
+		if owner == nil {
+			return nil
+		}
+		// ...make sure it's a Certificate...
+		if owner.APIVersion != apiGVStr || owner.Kind != "Certificate" {
+			return nil
+		}
+
+		// ...and if so, return it
+		return []string{owner.Name}
+	}); err != nil {
+		return err
+	}
+
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&tlsv1.Certificate{}).
+		Owns(&corev1.Secret{}).
+		Complete(r)
+}
---
+++ mercacertmonger/internal/controller/certificate_controller_helper.go
@@ -0,0 +1,90 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+	tlsv1 "igarridot/learning-k8s-controllers/mercacertmonger/api/v1"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+type CertificateCredentials struct {
+	key  string
+	cert string
+}
+
+// Obtain TLS cert
+func ObtainTLSCertificate(secret *corev1.Secret) *corev1.Secret {
+	secret.StringData["tls.crt"] = "cert.pem"
+	secret.StringData["tls.key"] = "cert.key"
+	return secret
+}
+
+// Secret builder
+func ConstructSecretForCertificate(certificate *tlsv1.Certificate, r *CertificateReconciler) (*corev1.Secret, error) {
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Labels:      make(map[string]string),
+			Annotations: make(map[string]string),
+			Name:        certificate.Name,
+			Namespace:   certificate.Namespace,
+		},
+		StringData: make(map[string]string),
+		Type:       "kubernetes.io/tls",
+	}
+	if err := ctrl.SetControllerReference(certificate, secret, r.Scheme); err != nil {
+		fmt.Println("Unable so set controller reference on child object", err)
+		return nil, err
+	}
+
+	return secret, nil
+}
+
+// Renewal business logic
+func CertificateNeedsToBeRenewed(now, expireTime int64, certificate tlsv1.Certificate) bool {
+	if expireTime-now < CertIsNearToExpire {
+		fmt.Println("Certificate", certificate.Name, "needs to be renewed")
+		return true
+	}
+	fmt.Println("Certificate", certificate.Name, "does not need to be renewed")
+	return false
+}
+
+// Fake timestamp manager
+func CertificateTimestampManager(now int64, certificate *tlsv1.Certificate) *tlsv1.Certificate {
+	certificate.Status.ValidFrom = now
+	certificate.Status.ValidTo = now + CertRenewalPeriod
+	return certificate
+}
+
+// Update Certificate Status
+func UpdateCertificateStatus(r *CertificateReconciler, ctx context.Context, certificate *tlsv1.Certificate) (ctrl.Result, error) {
+	err := r.Status().Update(ctx, certificate)
+	if err != nil {
+		fmt.Println(err, "Unable to update Certificate", certificate.Name, "status")
+		return ctrl.Result{}, err
+	}
+	return ctrl.Result{}, nil
+}
+
+// Create new Certificate secret
+func CreateNewCertificateSecret(r *CertificateReconciler, ctx context.Context, secret *corev1.Secret, certificate *tlsv1.Certificate) (ctrl.Result, error) {
+	err := r.Create(ctx, secret)
+	if err != nil {
+		fmt.Println(err, "Unable to create secret for Certificate", certificate.Name, "secret", secret)
+		return ctrl.Result{}, err
+	}
+	return ctrl.Result{}, nil
+}
+
+// Update already existing Certificate
+func RenewCertificate(r *CertificateReconciler, ctx context.Context, secret *corev1.Secret, certificate *tlsv1.Certificate) (ctrl.Result, error) {
+	err := r.Update(ctx, secret)
+	if err != nil {
+		fmt.Println(err, "Unable to create secret for Certificate", certificate.Name, "secret", secret)
+		return ctrl.Result{}, err
+	}
+	return ctrl.Result{}, nil
+}
